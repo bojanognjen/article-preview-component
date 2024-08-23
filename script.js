cc92c0ec670bc1d124b3b86ef67ca1668406ad72
Bojan Lukic <bojan.ognjen@gmail.com>
2024-08-23 02:38:49 +0200

    finished project independently

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const share = document.querySelector('#share');
+const share = document.querySelector('.share');
+const shareIcons = document.querySelectorAll('.share-icon');
 
-const shareIcon = document.querySelectorAll('.share-icon');
-
-for (let i = 0; i < shareIcon.length; i++) {
-    shareIcon[i].addEventListener('click', () => {
-        share.classList.value == "share slide-top" 
-        ? (share.classList.value = "share slide-out-bottom")
-        : (share.classList.value = "share slide-top");
-
+for (let i = 0; i < shareIcons.length; i++) {
+    shareIcons[i].addEventListener('click', ()=> {
+        share.classList.value == 'share slide-out-bottom'
+        ? share.classList.value = 'share slide-top'
+        : share.classList.value = 'share slide-out-bottom';
     })
 }
